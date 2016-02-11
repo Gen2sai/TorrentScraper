519bc42460618dd60b3ee9bd127bfc934a4c2bc1
Genryu <Genryu@gmail.com>
2016-02-11 17:00:48 +0800

    Resolution Enum for better code consistency.

---
+++ TorrentScraper/TorrentScraper/Enum/ResolutionEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TorrentScraper.Enum
+{
+    public enum resolutionEnum
+    {
+        LowRes = 0,
+        MidRes = 1,
+        HiRes = 2
+    }
+}
