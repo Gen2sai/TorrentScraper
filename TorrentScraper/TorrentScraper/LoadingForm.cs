4ba0a85e87f5524dce177166d7a80745c7c1ac74
Gen2sai <PWRChong@hotmail.com>
2016-02-11 20:36:17 +0800

    all files which i forgotten

---
+++ TorrentScraper/TorrentScraper/LoadingForm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TorrentScraper
+{
+    public partial class LoadingForm : Form
+    {
+        public int i = 0;
+        public LoadingForm()
+        {
+            InitializeComponent();
+            //FormBorderStyle = FormBorderStyle.None;
+            ControlBox = false;
+            ShowIcon = false;
+            pictureBox1.ImageLocation = @"C:\Users\yt.chong\Desktop\GitHub\TorrentScraper\TorrentScraper\TorrentScraper\img\loading.gif";
+            Timer t = new Timer();
+            t.Tick += new EventHandler(loadingText);
+            t.Interval = 1000;
+            t.Start();
+        }
+
+        private void loadingText(object sender, EventArgs e)
+        {
+            if (i == 0)
+            {
+                label1.Text = "Loading .";
+                i++;
+            }
+            else if (i == 1)
+            {
+                label1.Text = "Loading ..";
+                i++;
+            }
+            else
+            {
+                label1.Text = "Loading ...";
+                i = 0;
+            }
+        }
+
+    }
+}
